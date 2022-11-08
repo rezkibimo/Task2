072506da5277f264a36a99bd7ddf297094ebcbab
rezki bimo <rezkibimo@users.noreply.github.com>
2022-11-08 19:41:01 +1100

    added order by function for query

--- SQL/querry.sql
+++ SQL/querry.sql
@@ -1,3 +1,4 @@
 SELECT brokerTable.broker_name AS BrokerName, COUNT(customerTable.broker_id) AS TotalBrokered FROM customerTable
 JOIN brokerTable ON customerTable.broker_id = brokerTable.broker_id
-GROUP BY brokerTable.broker_name;
+GROUP BY brokerTable.broker_name
+ORDER BY TotalBroker DESC;
