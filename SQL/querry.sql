ca7e53dac0a912c70968acea6085b2bd0d050682
rezki bimo <rezkibimo@users.noreply.github.com>
2022-11-08 19:39:53 +1100

    updated Query

--- SQL/querry.sql
+++ SQL/querry.sql
@@ -1,3 +1,3 @@
-SELECT Employees.FirstName,COUNT(Orders.EmployeeID) AS NumberOfSales FROM Orders
-JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
-GROUP BY Employees.FirstName;
+SELECT brokerTable.broker_name AS BrokerName, COUNT(customerTable.broker_id) AS TotalBrokered FROM customerTable
+JOIN brokerTable ON customerTable.broker_id = brokerTable.broker_id
+GROUP BY brokerTable.broker_name;
