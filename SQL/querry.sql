d75f612b6cf4d9da35074fbe8740c23622319074
rezki bimo <rezkibimo@users.noreply.github.com>
2022-11-08 19:35:03 +1100

    SQL Query Test 1

--- SQL/querry.sql
+++ SQL/querry.sql
@@ -1,1 +1,3 @@
-SELECT * FROM sales_database;
+SELECT Employees.FirstName,COUNT(Orders.EmployeeID) AS NumberOfSales FROM Orders
+JOIN Employees ON Orders.EmployeeID = Employees.EmployeeID
+GROUP BY Employees.FirstName;
